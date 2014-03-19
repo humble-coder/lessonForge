b2ba7a3b7e2ca5a5164ff694d9f0b91893bb0610
Mark A Brown <mark.philosophe@gmail.com>
2014-03-19 12:57:00 -0400

    refactored some code and improved user deletion of answers

--- app/assets/javascripts/controllers/lesson/lesson_edit_controller.js
+++ app/assets/javascripts/controllers/lesson/lesson_edit_controller.js
@@ -18,10 +18,8 @@ App.LessonEditController = Ember.ObjectController.extend({
 		addAnswer: function(question) {
 			answers = question.get('answers');
 			answer = this.store.createRecord('answer');
-			this.store.find('question', question.id).then(function(question) {
-				answer.set('question_id', question.id);
-				answer.set('question', question);
-			});
+			answer.set('question_id', question.id);
+			answer.set('question', question);
 			answers.pushObject(answer);
 			this.get('controllers.answer').set('isEditing', true);
 		},
@@ -29,10 +27,8 @@ App.LessonEditController = Ember.ObjectController.extend({
 		addQuestion: function(lesson) {
 			questions = lesson.get('questions');
 			question = this.store.createRecord('question');
-			this.store.find('lesson', lesson.id).then(function(lesson) {
-				question.set('lesson_id', lesson.id);
-				question.set('lesson', lesson);
-			});
+			question.set('lesson_id', lesson.id);
+			question.set('lesson', lesson);
 			questions.pushObject(question);
 			this.get('controllers.question').set('isEditing', true);
 		},
@@ -42,8 +38,8 @@ App.LessonEditController = Ember.ObjectController.extend({
 		},
 
 		saveQuestion: function(question) {
-			question.save();
-			this.get('controllers.question').set('isEditing', false);
+			var self = this;
+			question.save().then(self.get('controllers.question').set('isEditing', false));
 		},
 
 		editQuestion: function() {
@@ -51,23 +47,24 @@ App.LessonEditController = Ember.ObjectController.extend({
 		},
 
 		removeQuestion: function(question) {
-			var self = this;
 			var lesson = this.get('model');
 			questions = lesson.get('questions');
 			questions.removeObject(question);
-			question.destroyRecord().then(self.transitionToRoute('lesson.edit'));
+			question.destroyRecord();
 		},
 
 		saveAnswer: function(answer) {
-			answer.save();
-			this.get('controllers.answer').set('isEditing', false);
+			var self = this;
+			answer.save().then(self.get('controllers.answer').set('isEditing'), false);
 		},
 
 		editAnswer: function() {
 			this.get('controllers.answer').set('isEditing', true);
 		},
 
-		removeAnswer: function(answer) {
+		removeAnswer: function(question, answer) {
+			answers = question.get('answers');
+			answers.removeObject(answer);
 			answer.destroyRecord();
 		}
 	}
