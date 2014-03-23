a8239bd9662b624e7339b35816d362166e9c5a63
Mark A Brown <mark.philosophe@gmail.com>
2014-03-22 22:51:52 -0400

    added question- and answer-editing test

---
+++ test/javascripts/integration/teacher/teacher_edits_question_and_answer_content_test.js
@@ -0,0 +1,73 @@
+module("Teacher edits question and answer content", {
+	setup: function() {
+		App.reset();
+	}
+});
+
+
+test("successfully", function() {
+
+	visit('/').then(function() {
+		click("#new-user");
+		andThen(function() {
+			fillIn("#full-name", "Bob");
+			fillIn("#email-address", "bob13@something.com");
+			fillIn("#username", "bob13");
+			fillIn("#password", "something");
+			fillIn("#password-confirmation", "something");
+			click("#new-teacher");
+			click("#create-user");
+			andThen(function() {
+				fillIn("#username-or-email", "bob13");
+				fillIn("#login-password", "something");
+				click("#new-session");
+				andThen(function() {
+					click(".new-course-button");
+					fillIn("#new-course-name", "Course with a Lesson");
+					click("#save-course");
+					andThen(function() {
+						click("#view-lessons");
+						andThen(function() {
+							click("#new-lesson");
+							andThen(function() {
+								fillIn("#new-lesson-name", "New Lesson");
+								click("#save-lesson");
+								andThen(function() {
+									click("#edit-lesson");
+									andThen(function() {
+										click("#new-question");
+										andThen(function() {
+											fillIn(".question-content", "New Question");
+											andThen(function() {
+												click(".save-question");
+												ok(exists(".edit-question"), "Lesson view has question-edit button");
+												click(".edit-question");
+												fillIn(".question-content", "Updated Question");
+												click(".save-question");
+												click(".add-answer");
+												fillIn(".answer-content", "New Answer");
+												click(".save-answer");
+												andThen(function() {
+													ok(exists(".edit-answer"), "Lesson view has answer-edit button.");
+													click(".edit-answer");
+													fillIn(".answer-content", "Updated Answer");
+													click(".save-answer");
+													click("#done");
+													andThen(function() {
+														ok(exists("span:contains('Updated Question')"), "Lesson view has edited question displayed.");
+														ok(exists("div:contains('Updated Answer')"), "Lesson view has edited answer displayed.");
+														click("#logout");
+													});
+												});
+											});
+										});
+									});
+								});
+							});
+        		});
+      		});
+    		});
+  		});
+		});
+	});
+});
