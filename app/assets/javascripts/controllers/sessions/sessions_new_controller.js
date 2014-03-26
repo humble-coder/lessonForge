606dc62fefa7df974b8d8bfcbc3ce60f53f2e216
Mark A Brown <mark.philosophe@gmail.com>
2014-03-26 11:46:55 -0400

    experimented a bit with modifying sessions new controller

--- app/assets/javascripts/controllers/sessions/sessions_new_controller.js
+++ app/assets/javascripts/controllers/sessions/sessions_new_controller.js
@@ -17,7 +17,7 @@ App.SessionsNewController = Ember.ObjectController.extend(Ember.Validations.Mixi
 	}.property('username_or_email', 'password'),
 
 	actions: {
-		loginUser: function() {
+		loginUser: function(session) {
 			var self = this;
 			var router = this.get('target');
 			var data = this.getProperties('username_or_email', 'password');
